1a431578b152adc4761d291f6a2aef80e5da4ee2
Erdogan Taskesen <erdogant@gmail.com>
2020-09-27 12:33:45 +0200

    update unit test

--- tests/test_benfordslaw.py
+++ tests/test_benfordslaw.py
@@ -1,7 +1,8 @@
 import numpy as np
-import benfordslaw as bl
+from benfordslaw import benfordslaw
 
 def test_benfordslaw():
+    bl = benfordslaw()
     df = bl.import_example()
     Iloc = df['candidate']=='Donald Trump'
     X = df['votes'].loc[Iloc].values
@@ -9,17 +10,19 @@ def test_benfordslaw():
     # TEST 1: check output is unchanged
     methods=[None, 'chi2', 'ks']
     for method in methods:
-        out = bl.fit(X, method=method)
-        assert [*out.keys()]==['P', 't', 'alpha', 'method', 'percentage_emp']
+        bl = benfordslaw(method=method)
+        out = bl.fit(X)
+        assert [*out.keys()]==['P', 't', 'percentage_emp']
 
     # TEST 2: check chi2
-    out = bl.fit(X, method='chi2')
+    bl = benfordslaw(method='chi2')
+    out = bl.fit(X)
     assert out['P']==0.44441032315455364
     assert out['t']==7.888769391226363
     # TEST 3: check ks
-    out = bl.fit(X, method='ks')
+    bl = benfordslaw(method='ks')
+    out = bl.fit(X)
     assert out['P']==1.0
     assert out['t']==0.1111111111111111
     # TEST 4: check percentages
-    assert out['percentage_emp']==[31.64521544487969, 16.508114157806382, 12.479015109121432, 9.820928931169558, 7.862339115836598, 6.3794068270845, 5.8477895914941245, 4.616675993284835, 4.8405148293228875]
-    
+    assert np.all(out['percentage_emp'][:,1]==[31.64521544487969, 16.508114157806382, 12.479015109121432, 9.820928931169558, 7.862339115836598, 6.3794068270845, 5.8477895914941245, 4.616675993284835, 4.8405148293228875])
