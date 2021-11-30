aa4b3e2fa0c88a5b01d70960001b64cede5d0769
erdogant <erdogant@gmail.com>
2021-11-30 19:06:41 +0100

    new version

--- benfordslaw/__init__.py
+++ benfordslaw/__init__.py
@@ -2,7 +2,7 @@ from benfordslaw.benfordslaw import benfordslaw
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 # module level doc-string
 __doc__ = """
