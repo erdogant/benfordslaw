89b626ab7ebfa538f543b5448f1d9b99d425a190
erdogant <erdogant@gmail.com>
2022-03-11 17:09:12 +0100

    update unit test because rounding is removed

--- benfordslaw/tests/test_benfordslaw.py
+++ benfordslaw/tests/test_benfordslaw.py
@@ -23,13 +23,15 @@ class TestCLUSTIMAGE(unittest.TestCase):
 		# TEST 2: check chi2
 		bl = benfordslaw(method='chi2')
 		out = bl.fit(X)
-		assert out['P']==0.42298423856214706
-		assert out['t']==8.10808210809422
+		assert out['P']==0.4150723856863902
+		assert out['t']==8.190646662956185
 		assert out['P_significant']==False
+
 		# TEST 3: check ks
 		bl = benfordslaw(method='ks')
 		out = bl.fit(X)
 		assert out['P']==1.0
 		assert out['t']==0.1111111111111111
+
 		# TEST 4: check percentages
 		assert np.all(out['percentage_emp'][:,1]==[31.64521544487969, 16.508114157806382, 12.479015109121432, 9.820928931169558, 7.862339115836598, 6.3794068270845, 5.8477895914941245, 4.616675993284835, 4.8405148293228875])
