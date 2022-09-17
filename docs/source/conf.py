6854eebc708c25ce2606094693d16463e9d581b9
erdogant <erdogant@gmail.com>
2022-09-17 20:11:07 +0200

    update sphinx pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,10 @@ import sys
 sys.path.insert(0, os.path.abspath('../../'))
 import benfordslaw
 
+# -- Download rst file -----------------------------------------------------
+from urllib.request import urlretrieve
+urlretrieve ("https://erdogant.github.io/docs/rst/sponsor.rst", "sponsor.rst")
+
 # -- Project information -----------------------------------------------------
 
 project = 'benfordslaw'
