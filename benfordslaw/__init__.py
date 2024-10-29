f87108e440b4c78d421535c6a0c990d0dedf2c89
erdogant <erdogant@gmail.com>
2024-10-29 23:35:13 +0100

    new version

--- benfordslaw/__init__.py
+++ benfordslaw/__init__.py
@@ -2,12 +2,12 @@ from benfordslaw.benfordslaw import benfordslaw
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.3.0'
+__version__ = '2.0.0'
 
 # module level doc-string
 __doc__ = """
 benfordslaw is a python library to test the frequency distribution of leading digits.
-=====================================================================
+=====================================================================================
 
 Test if an empirical (observed) distribution differs significantly from a theoretical (expected, Benfords) distribution.
 The law states that in many naturally occurring collections of numbers, the leading significant digit is likely to be small.
